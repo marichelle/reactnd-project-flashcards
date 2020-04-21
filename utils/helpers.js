41eff30acf29c7e7e955ae70a6e55465658ab38d
Maya Estalilla <mayaestalilla@gmail.com>
2020-04-21 01:29:33 -0400

    Resolve notification issues

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,28 +2,39 @@ import { AsyncStorage } from 'react-native';
 import { Notifications } from 'expo';
 import * as Permissions from 'expo-permissions';
 
+import { danger } from './theme';
+
+const CHANNEL_ID = 'daily-notifications';
 const NOTIFICATION_KEY = 'UdaciCards:notifications';
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
+    Notifications.cancelAllScheduledNotificationsAsync
+  );
+}
+
+function createChannel() {
+  return {
+    name: 'Study Reminder',
+    description: "Don't forget to study today!",
+    priority: 'high',
+    sound: true,
+  };
+}
 
 function createNotification() {
   return {
-    title: 'Study Reminder!',
+    title: 'Study Reminder',
     body: "Don't forget to study today!",
     ios: {
       sound: true,
     },
     android: {
-      priority: 'high',
-      sound: true,
+      channelId: CHANNEL_ID,
+      color: danger,
       sticky: false,
-      vibrate: true,
     },
   };
-}
-
-export function clearLocalNotification() {
-  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
-    Notifications.cancelScheduledNotificationAsync
-  );
 }
 
 export function setLocalNotification() {
@@ -33,19 +44,28 @@ export function setLocalNotification() {
       if (data === null) {
         Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
           if (status === 'granted') {
-            Notifications.cancelScheduledNotificationAsync();
+            Notifications.createChannelAndroidAsync(CHANNEL_ID, createChannel())
+              .then(() => {
+                Notifications.cancelAllScheduledNotificationsAsync();
 
-            let tomorrow = new Date();
-            tomorrow.setDate(tomorrow.getDate() + 1);
-            tomorrow.setHours(20);
-            tomorrow.setMinutes(0);
+                const tomorrow = new Date();
+                tomorrow.setDate(tomorrow.getDate() + 1);
+                tomorrow.setHours(20);
+                tomorrow.setMinutes(0);
 
-            Notifications.scheduleLocalNotificationAsync(createNotification(), {
-              time: tomorrow,
-              repeat: 'day',
-            });
+                Notifications.scheduleLocalNotificationAsync(
+                  createNotification(),
+                  {
+                    time: tomorrow,
+                    repeat: 'day',
+                  }
+                );
 
-            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+                AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+              })
+              .catch((err) => {
+                console.log('err', err);
+              });
           }
         });
       }
